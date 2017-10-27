366350611e6be55be78a84ab7bfe6f88a01f4b89
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2017-10-27 13:07:28 +0100

    Add files via upload

--- R/gi_functions.R
+++ R/gi_functions.R
@@ -7,7 +7,7 @@
 # e.g. C000239 is the code for Alzheimer's. Using the code is less error prone than typing in disease name.
 # The restricted list of drugs we cant use is passed to this function.
 get_drugs <- function(umls,rlist) {
-  ilist <- filter(indications, umls_cui_from_meddra == umls)
+  ilist <- filter(indications, umls_cui_from_meddra == umls) 
   ilist <- setdiff(ilist$drugbank_name,rlist$drugbank_name)
   if(length(ilist) > 0){
     for (j in 1:length(ilist)){
@@ -18,3 +18,22 @@ get_drugs <- function(umls,rlist) {
     cat("\n","Sorry, no drugs found...check umls code is correct for your disease")
     return(NULL)}
 }
+
+get_drugs_plus <- function(umls,rlist) {
+  ilist <- filter(indications, umls_cui_from_meddra == umls) 
+  mydrugs <- ilist # temp storage
+  ilist <- setdiff(ilist$drugbank_name,rlist$drugbank_name)
+  ilist <- filter(mydrugs,drugbank_name == ilist)
+    
+  if(length(ilist) > 0){
+    for (j in 1:length(ilist)){
+      cat("\ndrug",j,"is", ilist[j])
+    }
+    return(ilist)
+  }else{
+    cat("\n","Sorry, no drugs found...check umls code is correct for your disease")
+    return(NULL)}
+}
+
+
+
