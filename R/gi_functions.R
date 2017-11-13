cc16bfab81e31c84ac5f58c520cdf5c4a2d669d5
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2017-11-13 12:35:29 +0000

    Add files via upload

--- R/gi_functions.R
+++ R/gi_functions.R
@@ -21,7 +21,6 @@ get_drug_names <- function(umls,rlist) {
 
 # get_drugs_plus() assumes that "indications" dataframe is already loaded. You must provide getdrugs() 
 # with the "umls_cui_from_meddra" code for your disease. It will return the drugs known to be used...
-# e.g. C000239 is the code for Alzheimer's. Using the code is less error prone than typing in disease name.
 # The restricted list of drugs we cant use is passed to this function.
 # It returns the drug names plus other information.
 get_drugs_plus <- function(umls,rlist) {
@@ -29,9 +28,8 @@ get_drugs_plus <- function(umls,rlist) {
   mydrugs <- ilist # temp storage
   ilist <- setdiff(ilist$drugbank_name,rlist$drugbank_name)
   
-  if(length(ilist) == 0){
+  if(length(ilist) == 0){  # stuck in a 2nd null test as zero length data creeping in and causing crashes.
     cat("\n","No drugs found...check umls code is correct for your disease or maybe no drugs for this disease")
-    #cat("\n i is...",j)
     return(NULL)}
   
     options(warn=-1)
@@ -62,7 +60,7 @@ id2umls <- function(id){
     tempy <- filter(mappings, meshId == x[i])  # if tempy has meshid then keep, else ignore and do save it
     if(nrow(tempy) > 0){
       y[i,] <- tempy[1,]
-      y[i,]$umls <- gsub("umls:","",y[i,]$umls)
+      y[i,]$umls <- gsub("umls:","",y[i,]$umls) # remove "umls:" from string
       #cat("\ni is ...",i)
     }
    
@@ -72,6 +70,29 @@ id2umls <- function(id){
 }
 
 
+# Adds the MESH code into drugs_list dataframe, useful info when relating drugs to disease categories
+# 
+add_meshcode <- function(drugs){
+  x <- vector(mode="character",length=nrow(drugs))
+  y <- vector(mode="character",length=nrow(drugs))
+  #drugplus <- 0
+  
+  for (i in 1:nrow(drugs)){
+    tempx <- paste("umls:",drugs[i,3],sep="") # add the bloody "umls:" back, and magic numbers: beware
+    tempy <- filter(mappings, umls == tempx) 
+    tempz <- filter(digestive, ID == tempy$meshId)
+    x[i] <- tempz$MeSH
+    y[i] <- tempz$ID
+  }
+  
+  drugsplus <- cbind(drugs,x)   # add the x or Mesh values
+  drugsplus <- cbind(drugsplus,y)   # add the y or ID values
+  
+  colnames(drugsplus)[5] <- "MeSH"
+  colnames(drugsplus)[6] <- "ID"
+  
+  return(drugsplus)
+}
 
 
 
