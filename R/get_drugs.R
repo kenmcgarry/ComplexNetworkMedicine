4f5982d5fe4ac5b420e3fbea8a998866ab6fb445
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2017-11-14 07:59:50 +0000

    Add files via upload

--- R/get_drugs.R
+++ R/get_drugs.R
@@ -9,17 +9,6 @@ source("gi_functions.R")  # load in the functions required for finding lists of 
 
 # Create a structure to hold all known drugs treating your particular disease of interest
 # Obviously instantiate these functions before calling them.
-
-drug_list <- get_drug_names("C0002395",restrictedlist)  # umls code for Alzheimers
-drug_list <- get_drug_names("C0020179",restrictedlist)  # umls code for Huntingdons
-drug_list <- get_drug_names("C0149925",restrictedlist)  # umls code for small cell lung cancer
-drug_list <- get_drug_names("C0007131",restrictedlist)  # umls code for non small cell lung cancer
-drug_list <- get_drug_names("C0024141",restrictedlist)  # umls code for systemic lupus erythematosus
-drug_list <- get_drug_names("C0029408",restrictedlist)  # umls code for Osteoarthritis
-drug_list <- get_drug_names("C0003873",restrictedlist)  # umls code for Rheumatoid arthritis
-
-drug_list <- get_drugs_plus("C0013295",restrictedlist)  # Duodenal Ulcer.
-
 # work with following dataframes: indications; mappings; digestive; disgene; 
 # save(indications, restrictedlist, mappings, digestive, disgene, file = "C06disease.RData")
 # the key to linking these files is the "ID" in digestive also called "meshId" in mappings
@@ -41,7 +30,7 @@ for (i in 1:nrow(disease_umls)){
 
 drug_list <- add_meshcode(drug_list) # adds the old mesh code and unique id to drug_list, useful to relate drugs to diseases.
 
-
+unique(drug_list$drugbank_name) # how many unique drugs do we have?
 
 
 
